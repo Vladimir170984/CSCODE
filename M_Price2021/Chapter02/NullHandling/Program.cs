31827f492dd2c5e8159f15fa1de16ffb84ce2a64
Vladimir <kruz_korvin@mail.ru>
2022-02-11 14:28:12 +0300

    adding gitignore

--- M_Price2021/Chapter02/NullHandling/Program.cs
+++ M_Price2021/Chapter02/NullHandling/Program.cs
@@ -57,9 +57,10 @@ namespace HelloCS
             int numberOfApples = 12;
             decimal pricePerApple = 0.35M;
 
-            Console.WriteLine(format: "")
-
-
+            Console.WriteLine(format: "{0} apples costs {1:C}", arg0: numberOfApples, arg1: pricePerApple * numberOfApples);
+            string formatted = string.Format(format: "{0} apples costs {1:C}", arg0: numberOfApples, arg1: pricePerApple * numberOfApples);
+            //WriteToFile(formatted); // записывает строку в файл
+            Console.WriteLine($"{numberOfApples} apples costs {pricePerApple * numberOfApples:C}");
 
 
         }
